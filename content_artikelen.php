342b50c9ec766ba8163b57b4d9dd06ac9cb9f079
Kayan Meijer <kayanspam@live.nl>
2013-03-13 09:23:24 +0100

    artikelen

--- content_artikelen.php
+++ content_artikelen.php
@@ -1,4 +1,49 @@
-<div class="home_artikel">
+<form>
+	<div class="artikel_zoeken">
+		<div class="artikel_plaintext">
+			<table>
+				<tr>
+					<td>
+						Zoeken op
+					</td>
+
+					<td>
+						<input type="radio" name="keuze" value="naam">naam<br>
+						<input type="radio" name="keuze" value="categorie">categorie<br>
+						<input type="radio" name="keuze" value="prijs">titel<br>
+						<input type="radio" name="keuze" value="beschrijving">beschrijving<br>
+						<input type="radio" name="keuze" value="prijs">prijs<br>
+					</td>
+				</tr>
+			</table>
+		</div>
+
+
+
+
+
+
+
+
+		<div class="artikel_btnzoeken">
+			<input type="submit" name="zoeken" value="Zoeken"/>
+		</div>
+	</div> 
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="home_artikel">
 
 	<div class="product_categorie"> 
 		<i>categorie</i>
@@ -19,5 +64,4 @@
 	<div class="product_prijs">
 		<i>prijs</i>
 	</div>
-	
-</div>
+</div> -->
