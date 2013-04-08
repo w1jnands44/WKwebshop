305518c452bf548314fba3b7f52b11b057610972
Kayan Meijer <kayanspam@live.nl>
2013-04-08 09:39:58 +0200

    winkelwagen

--- content/content_afronden.php
+++ content/content_afronden.php
@@ -1,98 +1,58 @@
 <?php 
-	$username = $_SESSION['user_name'];
+    {
+        include 'connect.php';
 
-	include 'class.phpmailer.php';
+    	$username = $_SESSION['user_name'];
+        $klant_id = $_SESSION['klant_id'];
+        $totaalprijs = 0;
+        $totaalproducten = 0;
 
-	$query = "SELECT * FROM users u, klanten k WHERE u.klant_id = k.klant_id AND u.user_name = '$username'";
+        $datum = date("Y-m-d H:i:s");
 
-	$resultaat = mysql_query($query) or die (mysql_error()) ;
+        // bestellingen
+        $query = "INSERT INTO bestellingen (klant_id, best_datum) VALUES ('$klant_id', '$datum')"; 
+        mysql_query($query) or die (mysql_error());
 
-	while ($row = mysql_fetch_array($resultaat)) 
-	{
-		$klantemail = $row['klant_email'];
-	}
+        // het verkrijgen van het bestelling_id
+        $bestelling_id = mysql_insert_id();
 
-		$toad = $username;
+        /*$query = "SELECT * FROM bestellingen WHERE best_datum = '$datum'";
+        $resultaat = mysql_query($query) or die (mysql_error());
 
-        //user e-mail
-        $umail = $klantemail;
+        while ($row = mysql_fetch_array($resultaat)) 
+            {
+                $bestelling_id = $row['bestelling_id'];
+            }*/
 
-        // set datum 
-        $datum = date("d.m.Y H:i");
-        $tijd = date("H:i");
-
-        // set ip 
-        $ip = $_SERVER['REMOTE_ADDR'];
-
-        //opbouw mail 
-        $body = $_POST["message"] . "<br />Verstuurd op " . $datum . " via het ip " . $ip . "<br /><br />";
-
-        $mail = new PHPMailer();
-
-        $mail->IsSMTP(); // send via SMTP
-        $mail->Host = "<smtp addres bijv: smtp.google.com>";
-        $mail->Port = 587;
-        $mail->Mailer = "smtp";
-        $mail->SMTPAuth = true; // turn on SMTP authentication
-        $mail->SMTPSecure = "tls";
-        $mail->Username = "<e@mail here>"; // SMTP username
-        $mail->Password = "<password>"; // SMTP password
-
-        $mail->SetFrom($umail, $umail);
-        $mail->AddAddress($toad, $toad);
-        $mail->Subject = $_POST["subject"];
-        $mail->AltBody = "To view the message, please use an HTML compatible email viewer!";
-        $mail->MsgHTML($body);
-
-        if (!$mail->Send())
+        $cookie = $_COOKIE["wkwebshop_$username"];
+        $vars = explode(";", $cookie);
+        $counter = 0;
+        
+        while (count($vars) > $counter) 
         {
-            $message = 'Mailer Error: ' . $mail->ErrorInfo;
-        }
-        else
-        {
-            $message = '<div class="srv_message_block"><p>The email has been sent.<br />
-        A highly trained monkey is processing your e-mail as we speak.<br />
-        Expect to hear from the monkey very soon.<br />
-        Please click to go to the homepage: <a href="index.php">home</a></p>
-        </div>';
-        }
+            $query = "SELECT * FROM artikelen WHERE artikel_id = ".$vars[$counter];
+            $counter++;
+            $resultaat = mysql_query($query) or die (mysql_error());
+            echo $query;
+            echo $resultaat;
+            
+            while ($row = mysql_fetch_array($resultaat)) 
+            {
+                $artikel_id = $row['artikel_id'];
+                $artikel_aantal = $vars[$counter];
+                $artikel_prijs = $row['artikel_prijs'];
+
+                $totaalprijs += $vars[$counter] * $row['artikel_prijs'];
+                $totaalproducten += $vars[$counter];
+
+                // bestelling regel
+                $query = "INSERT INTO bestelling_regel(bestelling_id, artikel_id, bestreg_artikel_prijs, bestreg_aantal) VALUES ($bestelling_id,$artikel_id, $artikel_prijs, $artikel_aantal)";
+                $resultaat = mysql_query($query) or die (mysql_error());
+            }
+            $counter++;
+        }       
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  ?>
-
-
 
 Gefeliciteerd u heeft succesvol betaald.
 U krijgt een email met daarin de gegevens over uw bestelling.
