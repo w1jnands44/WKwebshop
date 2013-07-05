fc2cafad55fbf099836a903f720fb7a7df789630
Kayan Meijer <kayanspam@live.nl>
2013-07-05 13:31:01 +0200

    laatste aanpassingen

--- content/content_winkelwagen.php
+++ content/content_winkelwagen.php
@@ -22,6 +22,26 @@
 
 	if (isset($_POST['updateWinkelwagen']))
 	{
+
+		print_r($_POST);
+
+			foreach ($_POST as $var) 
+			{
+				echo $var;
+			}
+
+			/*print_r($_POST);
+			$cookie = $_COOKIE["wkwebshop_$username"];
+
+			$vars = explode(";", $cookie);
+
+			$counter = 0;
+
+			
+
+
+
+
 			$cookie = $_COOKIE["wkwebshop_$username"];
 
 			$vars = explode(";", $cookie);
@@ -31,26 +51,32 @@
 			
 			$counter = 0;
 
-			while (count($vars) > $counter) 
+		while (count($vars) > $counter) 
+		{
+			
+			$query = "SELECT * FROM artikelen WHERE artikel_id = ".$vars[$counter];
+			$counter++;
+			$resultaat = mysql_query($query) or die (mysql_error()) ;
+
+			while ($row = mysql_fetch_array($resultaat)) 
 			{
-				$query = "SELECT * FROM artikelen WHERE artikel_id = ".$vars[$counter];
-				$counter++;
-				$resultaat = mysql_query($query) or die (mysql_error()) ;
-
-				while ($row = mysql_fetch_array($resultaat)) 
-				{
-
-				}
+	
+				$totaalprijs = 0;
+				$totaalproducten = 0;
 
 				$totaalprijs += $vars[$counter] * $row['artikel_prijs'];
 				$totaalproducten += $vars[$counter];
+
 			}
-		$counter++; 
-	}
+			 
+			$counter++;
+			}*/
+		}	
 
 	
 	if(isset($username) && isset($_COOKIE["wkwebshop_$username"]))
 	{
+
 		$cookie = $_COOKIE["wkwebshop_$username"];
 
 		$vars = explode(";", $cookie);
@@ -69,6 +95,7 @@
 		{
 			
 			$query = "SELECT * FROM artikelen WHERE artikel_id = ".$vars[$counter];
+
 			$counter++;
 			$resultaat = mysql_query($query) or die (mysql_error()) ;
 
@@ -83,7 +110,7 @@
 					$image = $row['artikel_image1'];
 				}
 			?>
-
+				<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
 				<tr>
 					<td style="text-align:center;">
 						<?php echo $row['artikel_naam']; ?>
@@ -92,31 +119,10 @@
 						<?php echo "&euro;".$row['artikel_prijs']; ?>
 					</td>
 					<td style="text-align:center;">
-						<input id="inputNumber" name="" type="number" min="1" max="99" style="width:50px;" value="<?php echo $vars[$counter]; ?>"/>
+						<input id="inputNumber" type="number" name="<?php echo $row['artikel_id']; ?>" min="1" max="99" style="width:50px;" value="<?php echo $vars[$counter]; ?>"/>
 					</td>
 				</tr>
 			
-			
-				<!--<div class="home_artikel">
-									<div class="product_afbeelding">
-										<img class="afbeelding" alt="afbeelding" src="<?php echo $image; ?>"/>
-									</div>	
-
-									<div class="product_naam">
-										<b> <?php echo $row['artikel_naam']; ?></b>
-									</div>
-									<div class="product_beschrijving">
-										<b> <?php echo $row['artikel_beschrijving']; ?></b>
-									</div>
-									<div class="rechtsonder">
-										<div class="product_prijs">
-											<b> <?php echo "&euro;" .$row['artikel_prijs']; ?></b>
-										</div>
-										<div class="bestelbtn">
-											<b>Aantal: </b><?php echo $vars[$counter]; ?>
-										</div>
-									</div>
-								</div>-->
 			<?php 
 
 			$totaalprijs += $vars[$counter] * $row['artikel_prijs'];
@@ -135,14 +141,13 @@
 	}
 ?>
 <hr />
- <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
 	<table class="zoeken">
 		<tr>
 			<td>
 				<input type="submit" name="deleteWinkelwagen" value="Uw winkelwagen leeg maken."/>
 			</td>
 			<td>
-				<input type="submit" id="updateWinkelwagen" name="updateWinkelwagen" value="Winkelwagen bijwerken."
+				<input type="submit" id="updateWinkelwagen" name="updateWinkelwagen" value="Winkelwagen bijwerken."/>
 			</td>
 			<td>
 				<b>Totaal prijs: </b> <?php echo "&euro;". $totaalprijs; ?>
